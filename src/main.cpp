01b8e20c1117aca12ba6d108e4f51a9d8f135d22
fiserj <fiser87@gmail.com>
2022-10-27 14:31:21 +0100

    Added helper to create BGFX init structure.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,68 @@
-#include <bgfx/bgfx.h>
+#include <stdint.h>                    // *int*_t
+
+#include <bgfx/bgfx.h>                 // bgfx::*
+
+#include <bx/platform.h>               // BX_PLATFORM_*
 
 #define GLFW_INCLUDE_NONE
-#include <GLFW/glfw3.h>
+#include <GLFW/glfw3.h>                // glfw*
+
+#if BX_PLATFORM_LINUX
+#   define GLFW_EXPOSE_NATIVE_X11
+#   define GLFW_EXPOSE_NATIVE_GLX
+#elif BX_PLATFORM_OSX
+#   define GLFW_EXPOSE_NATIVE_COCOA
+#   define GLFW_EXPOSE_NATIVE_NSGL
+#elif BX_PLATFORM_WINDOWS
+#   define GLFW_EXPOSE_NATIVE_WIN32
+#   define GLFW_EXPOSE_NATIVE_WGL
+#endif
+#include <GLFW/glfw3native.h>          // glfwGetX11Display, glfwGet*Window
+
+#if BX_PLATFORM_OSX
+#   import <Cocoa/Cocoa.h>             // NSWindow
+#   import <QuartzCore/CAMetalLayer.h> // CAMetalLayer
+
+static CAMetalLayer* create_metal_layer(NSWindow* window)
+{
+    CAMetalLayer* layer = [CAMetalLayer layer];
+
+    window.contentView.layer     = layer;
+    window.contentView.wantsLayer = YES;
+
+    return layer;
+}
+
+#endif // BX_PLATFORM_OSX
+
+[[maybe_unused]] static bgfx::Init create_bgfx_init(GLFWwindow* window)
+{
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+
+    bgfx::Init init;
+    init.resolution.width  = uint32_t(width );
+    init.resolution.height = uint32_t(height);
+
+#if BX_PLATFORM_LINUX
+    init.type              = bgfx::RendererType::Vulkan;
+    init.platformData.ndt  = glfwGetX11Display();
+    init.platformData.nwh  = reinterpret_cast<void*>(uintptr_t(glfwGetX11Window(window)));
+
+#elif BX_PLATFORM_OSX
+    init.type              = bgfx::RendererType::Metal;
+    init.platformData.nwh  = create_metal_layer(static_cast<NSWindow*>(glfwGetCocoaWindow(window)));
+
+#elif BX_PLATFORM_WINDOWS
+    init.type              = bgfx::RendererType::Direct3D12;
+    init.platformData.nwh  = glfwGetWin32Window(window);
+
+#else
+#   error Unsupported platform.
+#endif
+
+    return init;
+}
 
 int main(int, char**)
 {
