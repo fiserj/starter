0a48de277593dde4b449083dc74ad245daf3e6e4
Jakub Fiser <fiser87@gmail.com>
2022-12-20 20:28:33 +0100

    Patched shaderc CMake build script.

--- third_party/cmake/shaderc.cmake
+++ third_party/cmake/shaderc.cmake
@@ -35,18 +35,10 @@ if(WITH_PREBUILT_SHADERC)
 endif() # WITH_PREBUILT_SHADERC
 
 if(APPLE)
-    set(SHADERC_TOOL_BUILD_SCRIPT "${SHADERC_TOOL_DIR}/osx/build.sh")
     set(SHADERC_TOOL_OUTPUT_BINARY "${SHADERC_TOOL_DIR}/osx/shaderc")
 elseif(WIN32)
-    set(SHADERC_TOOL_BUILD_SCRIPT "${SHADERC_TOOL_DIR}/windows/build.bat")
     set(SHADERC_TOOL_OUTPUT_BINARY "${SHADERC_TOOL_DIR}/windows/shaderc.exe")
-else()
-    if(NOT PREBUILT_SHADERC_AVAILABLE)
-        message(FATAL_ERROR "Script compiling shaderc binary does not yet exist for this platform.")
-    endif()
 endif()
-
-get_filename_component(SHADERC_TOOL_WORKING_DIR ${SHADERC_TOOL_BUILD_SCRIPT} DIRECTORY)
 
 if(PREBUILT_SHADERC_AVAILABLE)
     get_filename_component(SHADERC_TOOL_NAME ${SHADERC_TOOL_OUTPUT_BINARY} NAME)
@@ -55,14 +47,10 @@ if(PREBUILT_SHADERC_AVAILABLE)
         OUTPUT ${SHADERC_TOOL_OUTPUT_BINARY}
         COMMAND ${CMAKE_COMMAND} -E copy "${shaderc_prebuilt_SOURCE_DIR}/${SHADERC_TOOL_NAME}" ${SHADERC_TOOL_OUTPUT_BINARY}
     )
-else()
-    add_custom_command(
-        OUTPUT ${SHADERC_TOOL_OUTPUT_BINARY}
-        COMMAND ${SHADERC_TOOL_BUILD_SCRIPT}
-        WORKING_DIRECTORY ${SHADERC_TOOL_WORKING_DIR}
-    )
-endif()
 
-add_custom_target(shaderc
-    DEPENDS ${SHADERC_TOOL_OUTPUT_BINARY}
-)
+    add_custom_target(shaderc
+        DEPENDS ${SHADERC_TOOL_OUTPUT_BINARY}
+    )
+else()
+    include(cmake/shaderclib.cmake)
+endif()
